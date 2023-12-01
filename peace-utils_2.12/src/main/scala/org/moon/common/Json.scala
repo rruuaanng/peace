ae35b63e4d1383b4415213317face09fac741158
ruang <longjinyii@outlook.com>
2023-12-01 17:08:19 +0800

    添加了Json解析

--- peace-utils_2.12/src/main/scala/org/moon/common/Json.scala
+++ peace-utils_2.12/src/main/scala/org/moon/common/Json.scala
@@ -1,5 +1,7 @@
 package org.moon.common
 
+import io.circe.parser.parse
+
 object Json {
 
   /**
@@ -22,6 +24,29 @@ object Json {
               |""".stripMargin.stripLineEnd)
         .mkString(",")
     }}"
+  }
+
+  /**
+   * 将JSON字符串解析为Map
+   *
+   * @param jsonStr JSON字符串
+   * @return
+   */
+  def chain(jsonStr: String): Map[String, String] = {
+    val json = parse(jsonStr)
+    json match {
+      case Right(x) =>
+        // 将键和值合并为字符串
+        val keysList = x.hcursor.keys.get.mkString(",")
+        // 获取键列表和值列表
+        val keys = keysList.split(",")
+        // 返回一个Map，其中包含了Json中所有字段和值
+        keys.map(k => {
+          k -> x.hcursor.get[String](k).getOrElse("")
+        }).toMap
+      case Left(error) =>
+        Map.empty[String, String]
+    }
   }
 }
 
